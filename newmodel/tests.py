055b532d209909a8fb5146f19d7b14284fafb683
Efim Abrikosov <whitesilver@ya.ru>
2020-05-03 20:28:32 +0000

    minor bug fixes

--- newmodel/tests.py
+++ newmodel/tests.py
@@ -4,7 +4,12 @@
 import os
 
 import newmodel.util as util
-from tf.keras.callbacks import LambdaCallback
+from tensorflow.keras.callbacks import LambdaCallback
+from tensorflow.summary import create_file_writer, scalar
+
+from google.cloud import storage
+
+
 
 SIMILARITY_TEST_NAMES =  {
      'MC-30.tsv': 'MC',
@@ -24,14 +29,24 @@ def get_similarity_tests(job_dir):
     tests_path = os.path.join(job_dir, 'similarity_tests')
 
     if job_dir[:5] == 'gs://': # make the file available in the container
-        _, loc_path = util.split_gs_prefix(tests_path)
-        if not os.path.exists(loc_path):
+        # if tests are found in gcp bucket, overwrite local tests
+        # else attempt using local test AND upload them to Google Storage Bucket
+        
+        bucket_name, path_name = util.split_gs_prefix(tests_path)
+        client = storage.Client()
+        bucket = client.get_bucket(bucket_name[5:])
+        if storage.Blob(bucket=bucket, name=path_name).exists(client):
+            if os.path.exists(path_name):
+                os.remove(path_name)
             tests_path = util.download_from_gs(tests_path)
+        elif os.path.exists(path_name):
+            util.upload_to_gs(path_name, tests_path)
+            tests_path = path_name
         else:
-            tests_path = loc_path
+            return {}
 
     tests_list = os.listdir(tests_path)
-    tests_dict = {SIMILARITY_TEST_NAMES.get(test, test):os.path.join(tests_path, test): for test in tests_list if test[-4:] in ('.tsv', '.txt')}
+    tests_dict = {SIMILARITY_TEST_NAMES.get(test, test):os.path.join(tests_path, test) for test in tests_list if test[-4:] in ('.tsv', '.txt')}
 
     return tests_dict
 
@@ -40,7 +55,7 @@ def get_similarity_tests(job_dir):
 # TODO: pair pearson and spearman to avoid computation duplication
 def similarity_tests_callbacks(model, mode_list, metric_list, output_stat_list, tests_dict, job_dir, out_file = None):
     callback_list = []
-    log_dir = os.path.join(os.path.join(job_dir, 'logs')
+    log_dir = os.path.join(job_dir, 'logs')
 
     if out_file:
         if log_dir[:5] == 'gs://':
@@ -48,6 +63,8 @@ def similarity_tests_callbacks(model, mode_list, metric_list, output_stat_list, 
             out_path = os.path.join(loc_path, out_file)
         else:
             out_path = os.path.join(log_dir, out_file)
+        if not os.path.exists(os.path.dirname(out_path)):
+            os.makedirs(os.path.dirname(out_path), exist_ok = True)
         with open(out_path, 'w+') as out:
             out.write('#test_name\t')
             for output_stat in output_stat_list:
@@ -60,7 +77,7 @@ def similarity_tests_callbacks(model, mode_list, metric_list, output_stat_list, 
     for mode in mode_list:
         for metric in metric_list:
 
-            file_writer = tf.summary.create_file_writer(os.path.join(log_dir, f'metrics/{mode}_{metric}'))
+            file_writer = create_file_writer(os.path.join(log_dir, f'metrics/{mode}_{metric}'))
             def callback_func(epoch, logs):
                 for test_name, test_path in tests_dict.items():
                     if out_path:
@@ -70,7 +87,7 @@ def similarity_tests_callbacks(model, mode_list, metric_list, output_stat_list, 
                     # separate summary for each test and stat
                     for output_stat in output_stat_list:
                         res = model.similarity_test(test_path, mode=mode, metric=metric, output_stat=output_stat)
-                        tf.summary.scalar(test_name + '_' + output_stat, data = res[0], step = epoch)
+                        summary.scalar(test_name + '_' + output_stat, data = res[0], step = epoch)
 
                         if out_path: # record stat
                             with open(out_path, 'a') as out:
